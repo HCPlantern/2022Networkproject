9f732f63c5197818a7717c6a23eadc7aa72688d9
Plantern <1345010966@qq.com>
2022-04-17 17:17:14 +0800

    change status code in ResponseLine to Integer.

--- NetworkProject/src/main/java/com/nju/HttpClient/Components/Response/ResponseLine.java
+++ NetworkProject/src/main/java/com/nju/HttpClient/Components/Response/ResponseLine.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.nio.charset.StandardCharsets;
+
 //响应行
 //格式：版本[S]状态码[S]短语[回车换行]
 @Data
@@ -11,13 +13,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ResponseLine {
     private String version;
-    private String statusCode;
+    private Integer statusCode;
     private String reasonPhrase;
 
     public String toString(){
         return version+" "+statusCode+" "+reasonPhrase+"\r\n";
     }
     public byte[] toBytes(){
-        return null;
+        return toString().getBytes(StandardCharsets.UTF_8);
     }
 }
